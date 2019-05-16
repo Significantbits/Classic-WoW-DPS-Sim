957fc2bca49658d2b3c28a70489803599324c0c0
Zach Johnston <zjohnston@slynet.midsouth.biz.rr.com>
2019-05-16 14:47:43 -0400

    Added stats file

---
+++ stats.py
@@ -0,0 +1,12 @@
+# Data structure for characer stats
+# Classic WoW Stats: https://classic.wowhead.com/guides/classic-wow-stats-and-attributes-overview
+
+def create_character_stats:
+    stats = {}
+
+    # In the future hopefully this can be filled in based on gear
+    stats["Intellect"] = 12
+    stats["Strength"] = 12
+    stats["Agility"] = 12
+    stats["Stamina"] = 12
+    stats["Spirit"] = 12
